afbdc9c1cfa0c69f4e82100b557b8dc66401264d
cassaram09 <cassaram09@gmail.com>
2017-01-04 22:53:58 -0500

    Done.

--- components/Product.js
+++ components/Product.js
@@ -22,23 +22,26 @@ Product.propTypes = {
   name: React.PropTypes.string.isRequired,
   producer: React.PropTypes.string,
   hasWatermark: React.PropTypes.bool,
-  color: React.PropTypes.string.isRequired,
+  color: React.PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
   weight: weightCheck
 }
 
-module.exports = Product;
+function weightCheck(props, propName) {  
+  const weight = props[propName];
 
-
-function weightCheck(props, propName, componentName) {  
-  componentName = comopnentName || 'ANONYMOUS';
-
-  if (props[propName]) {
-    let value = props[propName];
-    if (typeof value === 'number') {
-        return value >= 80 && value <= 300 ? null : new Error(propName + ' in ' + componentName + " isn't in the right weight range.");
-    }
+  if (weight === undefined) {
+    return new Error('The `weight` prop is required.');
   }
 
-  // assume all ok
-  return null;
+  if (isNaN(weight)) {
+    return new Error('The `weight` prop is not a number.');
+  }
+
+  const isValidWeight = weight > 80 && weight < 300;
+
+  if (!isValidWeight) {
+    return new Error('The `weight` prop should range between 80 and 300.');
+  }
 }
+
+module.exports = Product;
